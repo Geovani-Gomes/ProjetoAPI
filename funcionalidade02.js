9573d7b3e5d9d5aaa77c0680dd0475e2c0e59120
Geovani-Gomes <geovani.gomes012@gmailcom>
2024-05-23 23:01:57 -0300

    primeiro commit

--- funcionalidade02.js
+++ funcionalidade02.js
@@ -1,15 +1,25 @@
-
+//cadastro do usuário administrador 
 let usuarios = [
     { nomeUsuario: 'admin', senha: 'admin123', papel: 'admin' },
     { nomeUsuario: 'usuario', senha: 'usuario123', papel: 'usuario' }
-];
-
-//a função de login verificará se o usuário possui o papel de "admin" antes de permitir o acesso. Se o usuário for administrador, o login será bem-sucedido, caso contrário, será recusado.
-function login(nomeUsuario, senha) {
+  ];
+  
+  function login(nomeUsuario, senha) {
     const usuario = usuarios.find(u => u.nomeUsuario === nomeUsuario && u.senha === senha && u.papel === 'admin');
     if (usuario) {
-        return usuario;
+      console.log(`Login bem-sucedido para o usuário: ${usuario.nomeUsuario}`);
+      return usuario;
     } else {
-        return null;
+      console.log(`Login falhou para o usuário: ${nomeUsuario}`);
+      return null;
     }
-}
+  }
+  
+  // Chamada da função
+  const usuarioLogado = login('admin', 'admin123');
+  if (usuarioLogado) {
+    console.log(`O usuário ${usuarioLogado.nomeUsuario} possui o papel de 'admin'`);
+  } else {
+    console.log("O usuário não possui o papel de 'admin'");
+  }
+  
